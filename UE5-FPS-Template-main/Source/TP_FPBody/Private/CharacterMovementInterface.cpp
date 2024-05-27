b9daa980cf3f0fe60956cd00ad56e5082aeaf0bf
hotlemon211 <130558753+hotlemon211@users.noreply.github.com>
2024-05-28 02:38:37 +0900

    Gun_Press
    press

--- UE5-FPS-Template-main/Source/TP_FPBody/Private/CharacterMovementInterface.cpp
+++
@@ -1,1 +0,0 @@
-#include "CharacterMovementInterface.h"
--- UE5-FPS-Template-main/Source/TP_FPBody/TP_FPBody.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TP_FPBody.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_FPBody, "TP_FPBody" );
